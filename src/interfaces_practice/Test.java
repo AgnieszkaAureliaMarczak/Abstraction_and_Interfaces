f286354b2bde999d72748cd6a596893673c2741e
AgnieszkaAureliaMarczak <agnieszka.aurelia.marczak@gmail.com>
2024-01-27 21:22:59 +0100

    Method access modifiers on interface

--- src/interfaces_practice/Test.java
+++ src/interfaces_practice/Test.java
@@ -3,6 +3,8 @@ package interfaces_practice;
 public class Test {
     public static void main(String[] args) {
         inFlight(new Jet());
+     //   OrbitEarth.log("Testing " + new Satellite());
+        orbit(new Satellite());
     }
 
     private static void inFlight(FlightEnabled flier) {
@@ -12,6 +14,12 @@ public class Test {
         if (flier instanceof Trackable tracked) {
             tracked.track();
         }
+        flier.land();
+    }
+
+    private static void orbit(OrbitEarth flier) {
+        flier.takeOff();
+        flier.fly();
         flier.land();
     }
 }
